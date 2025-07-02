832dd28df8c62e2c2c4317553bb3c16cf2749ec1
Rafe Loveday <rafe.loveday@icloud.com>
2025-07-02 13:10:47 +0100

    hero page content + initial animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,39 @@
 'use client'
-import Image from "next/image";
 import { useEffect, useRef } from "react";
 import styles from "./page.module.css";
 import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { gsap } from "gsap";
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(SplitText);
 
 export default function Home() {
   const containerRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    document.fonts.ready.then(() => {
+      gsap.set(".split", { opacity: 1 });
+  
+      let split;
+      SplitText.create(".split", {
+        type: "words,lines",
+        linesClass: "line",
+        autoSplit: true,
+        mask: "lines",
+        onSplit: (self) => {
+          split = gsap.from(self.lines, {
+            duration: 0.5,
+            yPercent: 100,
+            opacity: 0,
+            stagger: 0.1,
+            ease: "expo.out"
+          });
+          return split;
+        }
+      });
+    });
+  }, []);
+  
 
   useEffect(() => {
     if (!containerRef.current) return;
@@ -27,12 +54,26 @@ export default function Home() {
     loader.load('/models/server_racking_system.glb',
       function (gltf) {
         server = gltf.scene;
-        server.position.y = -2;
-        server.rotation.y = 0;
+        server.position.set(8, 0, 0);
+        server.rotation.y = Math.PI;
         scene.add(server);
+
+        gsap.to(server.position, {
+          duration: 1.2,
+          x: 0,
+          ease: "expo.out",
+          delay: 0.3
+        });
+
+        gsap.to(server.rotation, {
+          duration: 1.2,
+          y: 0,
+          ease: "expo.out",
+          delay: 0.3
+        });
+
         console.log(gltf.animations);
       },
-      function (xhr) {},
       function (error) {
         console.error('Error loading model:', error);
       }
@@ -46,8 +87,8 @@ export default function Home() {
     const ambientLight = new THREE.AmbientLight(0xffffff, 1.3);
     scene.add(ambientLight);
 
-    const topLight = new THREE.DirectionalLight(0xffffff, 1);
-    topLight.position.set(50,50,50);
+    const topLight = new THREE.DirectionalLight(0xffffff, 2);
+    topLight.position.set(0,0,0);
     scene.add(topLight);
 
     const reRender3D = () => {
@@ -60,7 +101,17 @@ export default function Home() {
   return (
     <div className={styles.HeroContainer}>
       <main>
-        
+        <div className={styles.HeroContent}>
+          <div className={styles.HeroTitle}>
+            <h1 className="split">The Most Efficient Nodes on Solana.</h1>
+          </div>
+          <div className={styles.HeroSubtitle}>
+            <h2 className="split">The first fully owned, performance-optimized RPC infrastructure built for Web3</h2>
+          </div>
+          <div className={styles.HeroButton}>
+            <button className="split">Deploy a Node Instantly</button>
+          </div>
+        </div>
       </main>
 
       <div className={styles.Container3D} ref={containerRef}></div>
